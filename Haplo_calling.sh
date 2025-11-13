43e3cbf4fbbf90b77e1f91ef4a5d6ad1546fafbe
Your Name <alaahamid29@gmail.com>
2025-11-13 23:57:52 +0100

    Update Haplo_calling.sh for GATK SNP calling

--- Haplo_calling.sh
+++ Haplo_calling.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 #SBATCH --job-name=HaploCaller
-#SBATCH --account=ec34
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --time=02:00:00
-#SBATCH --mem=4G
 #SBATCH --output=slurm-%j.out
+#SBATCH --time=01:00:00
+#SBATCH --mem=4G
+#SBATCH --account=ec34
 
-set -o errexit
-set -o nounset
-
-module --quiet purge
+# Load module environment
 module load BIOS-IN5410/HT-2023
 
-# The script expects the first argument to be the BAM filename
+# FILENAME is the first argument provided to sbatch
 FILENAME="$1"
 
-# Create gvcf directory in submit dir (safe)
+# Make gvcf directory in the submit folder
 mkdir -p $SUBMITDIR/gvcf
 
-# Run GATK HaplotypeCaller producing a gvcf for this file
+# Run HaplotypeCaller
 gatk HaplotypeCaller -R Orosv1mt.fasta \
-  -I "$FILENAME" --ploidy 1 -O gvcf/"$FILENAME".gvcf.gz -ERC GVCF \
-  2> gvcf/HaploCaller_"$FILENAME".out
+    -I $FILENAME --ploidy 1 \
+    -O gvcf/$FILENAME.gvcf.gz \
+    -ERC GVCF \
+    2> gvcf/HaploCaller_$FILENAME.out
 
 echo "$FILENAME HaplotypeCalled"
